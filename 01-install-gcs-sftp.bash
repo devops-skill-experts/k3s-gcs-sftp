6ba327add7411915442e81049e93a81bdf2e4457
pjames <pjames@gmail.com>
2023-08-30 21:26:38 +0700

    Fixed script

--- 01-install-gcs-sftp.bash
+++ 01-install-gcs-sftp.bash
@@ -3,6 +3,7 @@
 export KUBECONFIG=$(pwd)/kubeconfig.yaml
 NS=sftp
 
+kubectl create ns ${NS}
 kubectl create secret -n ${NS} generic sftp-gcloud-key --from-file=gcloud-key.json
 kubectl create secret -n ${NS} generic ssh-secret-key --from-file=ssh_host_rsa_key
 
