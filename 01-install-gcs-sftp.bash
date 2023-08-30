de674622d02e6f87819324aac2189a1473ee7fc2
pjames <pjames@gmail.com>
2023-08-30 21:12:43 +0700

    Fixed script

--- 01-install-gcs-sftp.bash
+++ 01-install-gcs-sftp.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+export KUBECONFIG=$(pwd)/kubeconfig.yaml
 NS=sftp
-cd "01-gcs-sftp"
 
 kubectl create secret -n ${NS} generic sftp-gcloud-key --from-file=gcloud-key.json
 kubectl create secret -n ${NS} generic ssh-secret-key --from-file=ssh_host_rsa_key
+
+cd "01-gcs-sftp"
 
 helm dependency update
 helm upgrade -i init . -n ${NS} --create-namespace
