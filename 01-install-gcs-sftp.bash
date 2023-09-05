f1a8bc1cc5fae91d884f5cb43ee8cde7386b34d4
pjames <pjames@gmail.com>
2023-09-05 10:47:51 +0700

    Auto create ssh private key file

--- 01-install-gcs-sftp.bash
+++ 01-install-gcs-sftp.bash
@@ -2,10 +2,13 @@
 
 export KUBECONFIG=$(pwd)/kubeconfig.yaml
 NS=sftp
+SSH_KEY=ssh_host_rsa_key
+
+ssh-keygen -t rsa -f ${SSH_KEY} -q -P ""
 
 kubectl create ns ${NS}
 kubectl create secret -n ${NS} generic sftp-gcloud-key --from-file=gcloud-key.json
-kubectl create secret -n ${NS} generic ssh-secret-key --from-file=ssh_host_rsa_key
+kubectl create secret -n ${NS} generic ssh-secret-key --from-file=${SSH_KEY}
 
 cd "01-gcs-sftp"
 
